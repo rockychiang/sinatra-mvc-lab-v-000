381b0b7dc9d4637c83862c6bec229a3a97741205
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-05 22:57:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,7 +3,6 @@ class PigLatinizer
   attr_reader :sentence, :word
   
   def piglatinize(text)
-    @text = text
     @sentence = text.split(" ")
     @sentence.map do |word|
       if word.downcase.start_with?("a","e","i","o","u")
